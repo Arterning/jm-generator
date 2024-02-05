e8dd7d8549c05b1685aaae1709a990d86ca89acb
Arterning <ning.huang.ridson@gmail.com>
2024-02-05 16:37:40 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
-const inquirer = require('inquirer');
+let inquirer = null;
+
+async function loadInquirer() {
+  const { default: inquirerModule } = await import('inquirer');
+  inquirer = inquirerModule;
+}
 const fs = require('fs');
 const path = require('path');
 
@@ -9,6 +14,7 @@ program
   .command('new <projectName>')
   .description('Create a new Java Maven project')
   .action(async (projectName) => {
+    await loadInquirer();
     const answers = await inquirer.prompt([
       {
         type: 'input',
@@ -32,7 +38,17 @@ program
     fs.writeFileSync(path.join(projectPath, 'pom.xml'), pomXmlContent);
 
     // Create src/main/java directory
-    fs.mkdirSync(path.join(projectPath, 'src', 'main', 'java'), { recursive: true });
+    const javaPackagePath = path.join(projectPath, 'src', 'main', 'java', ...answers.groupId.split('.'));
+    fs.mkdirSync(javaPackagePath, { recursive: true });
+
+    // Write Test.java file
+    const testJavaContent = generateTestJava(answers.groupId);
+    fs.writeFileSync(path.join(javaPackagePath, 'Test.java'), testJavaContent);
+
+
+    // Create README.md and write description
+    const readmeContent = `# Java Maven Project Generator\n\nThis command line tool helps you create a new Java Maven project with a specified Group ID and Artifact ID. It generates the necessary project structure, including the \`pom.xml\` file and a sample \`Test.java\` file with a 'Hello, World!' program.`;
+    fs.writeFileSync(path.join(projectPath, 'README.md'), readmeContent);
 
     console.log('Java Maven project created successfully.');
   });
@@ -47,4 +63,14 @@ function generatePomXml(groupId, artifactId) {
     <artifactId>${artifactId}</artifactId>
     <version>1.0.0</version>
   </project>`;
+}
+
+function generateTestJava(groupId) {
+  return `package ${groupId};
+
+  public class Test {
+    public static void main(String[] args) {
+      System.out.println("Hello, World!");
+    }
+  }`;
 }
