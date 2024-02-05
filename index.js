b289eb8cd19f6a90a3ecd8c16c703da2108e38ae
Arterning <ning.huang.ridson@gmail.com>
2024-02-05 22:52:13 +0800

    refactor

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
-const { createNormalProject } = require('./create-normal-project');
+const { program } = require("commander");
+const { createProject } = require("./action");
+
+program.option("-t, --template <template>", "Project template", "normal").parse();
 
 program
-  .command('new <projectName>')
-  .description('Create a new Java Maven project')
-  .action(createNormalProject);
+  .command("new <projectName>")
+  .description("Create a new Java Maven project")
+  .action(async (projectName) => {
+    createProject(projectName, program.opts())
+  });
 
 program.parse(process.argv);
-
--- utils.js
+++ utils.js
@@ -22,6 +22,85 @@ function generatePomXml(groupId, artifactId, jdkVersion) {
     </project>`;
 }
 
+function generateSpringbootPomXml(groupId, artifactId, jdkVersion) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+    <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+             xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+      <modelVersion>4.0.0</modelVersion>
+
+      <parent>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-parent</artifactId>
+        <version>3.1.8</version>
+        <relativePath/>
+      <!--  lookup parent from repository  -->
+      </parent>
+
+      <groupId>${groupId}</groupId>
+      <artifactId>${artifactId}</artifactId>
+      <name>${artifactId}</name>
+      <description>Demo project for Spring Boot</description>
+      <version>1.0.0</version>
+      <packaging>jar</packaging>
+
+      <properties>
+        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+        <maven.compiler.encoding>UTF-8</maven.compiler.encoding>
+        <java.version>${jdkVersion}</java.version>
+        <maven.compiler.source>${jdkVersion}</maven.compiler.source>
+        <maven.compiler.target>${jdkVersion}</maven.compiler.target>
+      </properties>
+
+      <dependencies>
+        <dependency>
+          <groupId>org.springframework.boot</groupId>
+          <artifactId>spring-boot-starter-data-jdbc</artifactId>
+        </dependency>
+        <dependency>
+          <groupId>org.springframework.boot</groupId>
+          <artifactId>spring-boot-starter-data-jpa</artifactId>
+        </dependency>
+
+        <dependency>
+          <groupId>org.springframework.boot</groupId>
+          <artifactId>spring-boot-starter-web</artifactId>
+        </dependency>
+
+        <dependency>
+          <groupId>com.mysql</groupId>
+          <artifactId>mysql-connector-j</artifactId>
+          <scope>runtime</scope>
+        </dependency>
+        <dependency>
+          <groupId>org.postgresql</groupId>
+          <artifactId>postgresql</artifactId>
+          <scope>runtime</scope>
+        </dependency>
+        <dependency>
+          <groupId>org.projectlombok</groupId>
+          <artifactId>lombok</artifactId>
+          <optional>true</optional>
+        </dependency>
+
+        <dependency>
+          <groupId>org.springframework.boot</groupId>
+          <artifactId>spring-boot-starter-test</artifactId>
+          <scope>test</scope>
+        </dependency>
+      </dependencies>
+
+
+      <build>
+        <plugins>
+          <plugin>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-maven-plugin</artifactId>
+          </plugin>
+        </plugins>
+      </build>
+    </project>`;
+}
+
 function generateAppJava(groupId) {
   return `package ${groupId};
   
@@ -33,6 +112,21 @@ function generateAppJava(groupId) {
     }`;
 }
 
+function generateSpringbootAppJava(groupId) {
+  return `package ${groupId};
+  
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+  
+@SpringBootApplication
+public class App {
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+}`;
+}
+
+
 function generateTestJava(groupId) {
   return `package ${groupId};
   
@@ -43,8 +137,26 @@ function generateTestJava(groupId) {
     }`;
 }
 
+function generateSpringbootTestJava(groupId) {
+  return `package ${groupId};
+  
+    import org.junit.jupiter.api.Test;
+    import org.springframework.boot.test.context.SpringBootTest;
+  
+    @SpringBootTest
+    class ContextTest {
+      @Test
+      void contextLoads() {
+        System.out.println("Hello, World!");
+      }
+    }`;
+}
+
 module.exports = {
   generatePomXml,
   generateAppJava,
   generateTestJava,
+  generateSpringbootPomXml,
+  generateSpringbootAppJava,
+  generateSpringbootTestJava
 };
